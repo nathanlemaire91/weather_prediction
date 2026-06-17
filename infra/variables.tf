5b1ba04128b725dc5ce77823d0198ca3d7df94a3
Nathan Lemaire <nathlem9129@hotmail.fr>
2026-06-17 14:58:31 +0200

    bucket name

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "bucket_hello_world"
+  default     = "weather-prediction-bucket-dev"
 }
 
 variable "region" {
