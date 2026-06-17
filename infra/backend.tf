5ac44a9b886972ee3d3c3171523607763e932c9d
Nathan Lemaire <nathlem9129@hotmail.fr>
2026-06-17 16:54:52 +0200

    change bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "weather-prediction-terraform-state"
     key            = "weather_prediction/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
