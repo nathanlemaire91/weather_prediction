76acd906f9da747459cd03f76ccd5e12a09a1bda
Nathan Lemaire <nathlem9129@hotmail.fr>
2026-06-17 16:50:36 +0200

    remove ressources

--- infra/storage.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "weather_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
-  }
-}
