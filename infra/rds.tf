23dd1144ca6d5849c9dcc0e07123c456ef5b734c
Nathan Lemaire <nathlem9129@hotmail.fr>
2026-06-17 15:51:12 +0200

    add database

--- infra/rds.tf
+++ infra/rds.tf
@@ -51,3 +51,4 @@ resource "aws_security_group" "weather-security-group" {
   db_subnet_group_name   = aws_db_subnet_group.weather-subnet.name
   vpc_security_group_ids = [aws_security_group.weather-security-group.id]
   parameter_group_name   = aws_db_parameter_group.weather-subnet.name
+}
